f3fc3676edecbec24e2d14372672c353277e9c14
27rikos <koncosolin09@gmail.com>
2025-01-04 22:30:44 +0700

    feat: add profile,add gallery

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\admin\BeritaController;
 use App\Http\Controllers\admin\DashboardController;
+use App\Http\Controllers\admin\EventController;
 use App\Http\Controllers\admin\FotoController;
+use App\Http\Controllers\admin\HistoryController;
 use App\Http\Controllers\Admin\KategoriController;
+use App\Http\Controllers\admin\ProfileController;
 use App\Http\Controllers\admin\ValidatingController;
 use App\Http\Controllers\admin\VideoController;
 use App\Http\Controllers\Auth\LoginController;
@@ -26,7 +29,7 @@ Route::get('/', function () {
 
 // public route
 Route::get('login', [LoginController::class, 'index'])->name('login');
-// admin route
+// private route:admin:
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::resource('kategori-berita', KategoriController::class);
 Route::resource('berita', BeritaController::class);
@@ -34,3 +37,11 @@ Route::get('publish/{id}', [ValidatingController::class, 'publish'])->name('publ
 Route::get('draft/{id}', [ValidatingController::class, 'draft'])->name('draft');
 Route::resource('photo', FotoController::class);
 Route::resource('videos', VideoController::class);
+Route::resource('event', EventController::class);
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+
+Route::get('histories/create', [HistoryController::class, 'create'])->name('history.create');
+Route::post('histories/store', [HistoryController::class, 'store'])->name('history.store');
+Route::get('histories/{id}/edit', [HistoryController::class, 'edit'])->name('history.edit');
+Route::put('histories/{id}/update', [HistoryController::class, 'update'])->name('history.update');
+Route::get('histories/{id}/destroy', [HistoryController::class, 'destroy'])->name('history.destroy');
